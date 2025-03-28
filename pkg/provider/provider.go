c3c02cbd93ca43c1543f7df3d4ac827c834d56d6
Christian Bianchi <whites11@gmail.com>
2025-03-28 11:31:55 +0100

    Add native clickhouse client implementation and provider config

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -2,52 +2,168 @@ package provider
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	tfresource "github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 
+	"github.com/ClickHouse/terraform-provider-clickhousedbops/internal/clickhouseclient"
+	"github.com/ClickHouse/terraform-provider-clickhousedbops/internal/rbac"
 	"github.com/ClickHouse/terraform-provider-clickhousedbops/pkg/project"
+	"github.com/ClickHouse/terraform-provider-clickhousedbops/pkg/resource/database"
+	"github.com/ClickHouse/terraform-provider-clickhousedbops/pkg/resource/role"
+	"github.com/ClickHouse/terraform-provider-clickhousedbops/pkg/resource/user"
 )
 
-// Ensure Provider satisfies various provider interfaces.
-var _ provider.Provider = &Provider{}
+const (
+	protocolNative       = "native"
+	protocolNativeSecure = "nativesecure"
 
-// Provider defines the provider implementation.
-type Provider struct{}
+	authStrategyPassword = "password"
+)
 
-func (p *Provider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+var (
+	availableProtocols      = []string{protocolNative, protocolNativeSecure}
+	availableAuthStrategies = []string{authStrategyPassword}
+)
+
+// Ensure ClickhouseRBACProvider satisfies various provider interfaces.
+var _ provider.Provider = &ClickhouseRBACProvider{}
+
+// ClickhouseRBACProvider defines the provider implementation.
+type ClickhouseRBACProvider struct{}
+
+func (p *ClickhouseRBACProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
 	resp.TypeName = "clickhousedbops"
 	resp.Version = project.Version()
 }
 
-func (p *Provider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+func (p *ClickhouseRBACProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{},
+		Attributes: map[string]schema.Attribute{
+			"protocol": schema.StringAttribute{
+				Required:    true,
+				Description: fmt.Sprintf("The protocol to use to connect to clickhouse instance. Valid options are: %s", strings.Join(availableProtocols, ", ")),
+				Validators: []validator.String{
+					stringvalidator.OneOf(availableProtocols...),
+				},
+			},
+			"host": schema.StringAttribute{
+				Required:    true,
+				Description: "The hostname to use to connect to the clickhouse instance",
+			},
+			"port": schema.NumberAttribute{
+				Required:    true,
+				Description: "The port to use to connect to the clickhouse instance",
+			},
+			"auth_config": schema.SingleNestedAttribute{
+				Attributes: map[string]schema.Attribute{
+					"strategy": schema.StringAttribute{
+						Required:    true,
+						Description: "The authentication method to use",
+						Validators: []validator.String{
+							stringvalidator.OneOf(availableAuthStrategies...),
+						},
+					},
+					"username": schema.StringAttribute{
+						Required:    true,
+						Description: "The username to use to authenticate to ClickHouse",
+						Validators: []validator.String{
+							stringvalidator.LengthAtLeast(1),
+						},
+					},
+					"password": schema.StringAttribute{
+						Optional:    true,
+						Description: "The password to use to authenticate to ClickHouse",
+						Validators: []validator.String{
+							stringvalidator.LengthAtLeast(1),
+						},
+					},
+				},
+				Required:    true,
+				Description: "Authentication configuration",
+			},
+		},
 	}
 }
 
-func (p *Provider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+func (p *ClickhouseRBACProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
 	var data Model
+	var err error
 
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
 
 	if resp.Diagnostics.HasError() {
 		return
 	}
+
+	var clickhouseClient clickhouseclient.ClickhouseClient
+	{
+		switch data.Protocol {
+		case protocolNative:
+			fallthrough
+		case protocolNativeSecure:
+			var auth *clickhouseclient.UserPasswordAuth
+			switch data.AuthConfig.Strategy {
+			case authStrategyPassword:
+				auth = &clickhouseclient.UserPasswordAuth{
+					Username: data.AuthConfig.Username,
+				}
+
+				if data.AuthConfig.Password != nil {
+					auth.Password = *data.AuthConfig.Password
+				}
+
+				valid, errorStrings := auth.ValidateConfig()
+				if !valid {
+					resp.Diagnostics.AddError("invalid configuration", fmt.Sprintf("invalid authentication strategy configuration. %s", strings.Join(errorStrings, ", ")))
+				}
+			default:
+				resp.Diagnostics.AddError("invalid configuration", fmt.Sprintf("invalid authentication strategy %q. %s protocol only supports %q", data.AuthConfig.Strategy, protocolNative, authStrategyPassword))
+				return
+			}
+
+			clickhouseClient, err = clickhouseclient.NewNativeClient(clickhouseclient.NativeClientConfig{
+				Host:             data.Host,
+				Port:             data.Port,
+				UserPasswordAuth: auth,
+				EnableTLS:        data.Protocol == protocolNativeSecure,
+			})
+		}
+	}
+
+	if err != nil {
+		resp.Diagnostics.AddError("error initializing query runner", fmt.Sprintf("%+v\n", err))
+	}
+
+	client, err := rbac.New(clickhouseClient)
+	if err != nil {
+		resp.Diagnostics.AddError("error initializing rbac client", fmt.Sprintf("%+v\n", err))
+	}
+
+	resp.DataSourceData = client
+	resp.ResourceData = client
 }
 
-func (p *Provider) Resources(ctx context.Context) []func() tfresource.Resource {
-	return []func() tfresource.Resource{}
+func (p *ClickhouseRBACProvider) Resources(ctx context.Context) []func() tfresource.Resource {
+	return []func() tfresource.Resource{
+		database.NewResource,
+		role.NewResource,
+		user.NewResource,
+	}
 }
 
-func (p *Provider) DataSources(ctx context.Context) []func() datasource.DataSource {
+func (p *ClickhouseRBACProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{}
 }
 
 func New() func() provider.Provider {
 	return func() provider.Provider {
-		return &Provider{}
+		return &ClickhouseRBACProvider{}
 	}
 }
