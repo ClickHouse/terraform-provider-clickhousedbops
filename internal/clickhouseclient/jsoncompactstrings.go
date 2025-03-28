38009ac9409f0b5d7f040d1c0c06a20299d4cdc7
Christian Bianchi <whites11@gmail.com>
2025-03-28 14:09:50 +0100

    Add http clickhouse client implementation

--- internal/clickhouseclient/jsoncompactstrings.go
+++ internal/clickhouseclient/jsoncompactstrings.go
@@ -1,5 +1,6 @@
 package clickhouseclient
 
+// jsonCompatStrings is used to parse clickhouse query output when using 'jsonCompatStrings' format.
 type jsonCompatStrings struct {
 	Meta []struct {
 		Name string
@@ -11,11 +12,13 @@ type jsonCompatStrings struct {
 func (j jsonCompatStrings) Rows() []Row {
 	ret := make([]Row, 0)
 
+	// Extract slice of column names in the result.
 	colNames := make([]string, 0)
 	for _, entry := range j.Meta {
 		colNames = append(colNames, entry.Name)
 	}
 
+	// Create a slice of Rows associating the value to each column name.
 	for _, row := range j.Data {
 		data := Row{}
 
