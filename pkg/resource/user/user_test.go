ecb4a123b1e7ddd00bb53c5602f61f55edf8dd2f
Mattias Sjödin <86059470+MattiasMTS@users.noreply.github.com>
2025-11-24 10:32:50 +0100

    feat: add `password_sha256_hash ` for `user` resource to support OpenTofu (#109)
    * feat: add password_sha256_hash field for user backward compatibility
    
    * test: add test for password_sha256_hash
    
    * docs: update docs for password_hash on user resource
    
    ---------
    
    Co-authored-by: Nuno Adrego <55922671+nunoadrego@users.noreply.github.com>

--- pkg/resource/user/user_test.go
+++ pkg/resource/user/user_test.go
@@ -3,11 +3,14 @@ package user_test
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 
 	"github.com/ClickHouse/terraform-provider-clickhousedbops/internal/dbops"
+	"github.com/ClickHouse/terraform-provider-clickhousedbops/internal/testutils/factories"
 	"github.com/ClickHouse/terraform-provider-clickhousedbops/internal/testutils/nilcompare"
 	"github.com/ClickHouse/terraform-provider-clickhousedbops/internal/testutils/resourcebuilder"
 	"github.com/ClickHouse/terraform-provider-clickhousedbops/internal/testutils/runner"
@@ -30,7 +33,7 @@ func TestUser_acceptance(t *testing.T) {
 		return user != nil, err
 	}
 
-	checkAttributesFunc := func(ctx context.Context, dbopsClient dbops.Client, clusterName *string, attrs map[string]interface{}) error {
+	checkAttributesFunc := func(ctx context.Context, dbopsClient dbops.Client, clusterName *string, attrs map[string]any) error {
 		id := attrs["id"]
 		if id == nil {
 			return fmt.Errorf("id was nil")
@@ -147,7 +150,88 @@ func TestUser_acceptance(t *testing.T) {
 			CheckNotExistsFunc:  checkNotExistsFunc,
 			CheckAttributesFunc: checkAttributesFunc,
 		},
+		{
+			Name:        "Create User with new password_sha256_hash field (OpenTofu version < 1.11 compatibility)",
+			ChEnv:       map[string]string{"CONFIGFILE": "config-single.xml"},
+			Protocol:    "native",
+			ClusterName: nil,
+			Resource: resourcebuilder.New(resourceType, resourceName).
+				WithStringAttribute("name", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)).
+				WithFunction("password_sha256_hash", "sha256", "changeme").
+				Build(),
+			ResourceName:        resourceName,
+			ResourceAddress:     fmt.Sprintf("%s.%s", resourceType, resourceName),
+			CheckNotExistsFunc:  checkNotExistsFunc,
+			CheckAttributesFunc: checkAttributesFunc,
+		},
+		{
+			Name:        "Create User with new password_sha256_hash field on cluster with localfile storage",
+			ChEnv:       map[string]string{"CONFIGFILE": "config-localfile.xml"},
+			Protocol:    "native",
+			ClusterName: &clusterName,
+			Resource: resourcebuilder.New(resourceType, resourceName).
+				WithStringAttribute("cluster_name", clusterName).
+				WithStringAttribute("name", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)).
+				WithFunction("password_sha256_hash", "sha256", "changeme").
+				Build(),
+			ResourceName:        resourceName,
+			ResourceAddress:     fmt.Sprintf("%s.%s", resourceType, resourceName),
+			CheckNotExistsFunc:  checkNotExistsFunc,
+			CheckAttributesFunc: checkAttributesFunc,
+		},
 	}
 
 	runner.RunTests(t, tests)
+}
+
+func TestUser_validation_acceptance(t *testing.T) {
+	providers := factories.ProviderFactories()
+	const sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+	tests := []resource.TestCase{
+		{
+			ProtoV6ProviderFactories: providers,
+			Steps: []resource.TestStep{
+				// test ensure you can't use both password_sha256_hash and password_sha256_hash_wo
+				{
+					Config: fmt.Sprintf(`
+					resource "clickhousedbops_user" "test" {
+						name                    = "testuser"
+						password_sha256_hash    = "%s"
+						password_sha256_hash_wo = "%s"
+						password_sha256_hash_wo_version = 1
+					}
+				`, sha256, sha256),
+					PlanOnly:    true,
+					ExpectError: regexp.MustCompile(`(?s)Invalid Attribute Combination.*password_sha256_hash.*cannot be specified`),
+				},
+				// test ensure you can't use password_sha256_hash_wo without specifying password_sha256_hash_wo_version
+				{
+					Config: fmt.Sprintf(`
+					resource "clickhousedbops_user" "test" {
+						name                    = "testuser"
+						password_sha256_hash_wo = "%s"
+					}
+				`, sha256),
+					PlanOnly:    true,
+					ExpectError: regexp.MustCompile(`(?s)Invalid Attribute Combination.*password_sha256_hash_wo_version.*must be specified when.*password_sha256_hash_wo.*is specified`),
+				},
+				// test ensure you can't use password_sha256_hash_wo_version without password_sha256_hash_wo
+				{
+					Config: `
+					resource "clickhousedbops_user" "test" {
+						name                            = "testuser"
+						password_sha256_hash_wo_version = 1
+					}
+				`,
+					PlanOnly:    true,
+					ExpectError: regexp.MustCompile(`(?s)Invalid Attribute Combination.*password_sha256_hash_wo.*must be specified when.*password_sha256_hash_wo_version.*is specified`),
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		resource.Test(t, tt)
+	}
 }
