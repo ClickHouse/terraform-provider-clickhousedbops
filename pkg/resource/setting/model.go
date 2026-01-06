ae183dcb3e3cb28db960ddf51033ddaab8c7c718
Nuno Adrego <55922671+nunoadrego@users.noreply.github.com>
2026-01-06 18:59:26 +0000

    fix(setting): add retries on creation (#130)
    * fix(setting): add retries on creation
    
    * fix: support configurable timeout

--- pkg/resource/setting/model.go
+++ pkg/resource/setting/model.go
@@ -1,15 +1,17 @@
 package setting
 
 import (
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 type Setting struct {
-	ClusterName       types.String `tfsdk:"cluster_name"`
-	SettingsProfileID types.String `tfsdk:"settings_profile_id"`
-	Name              types.String `tfsdk:"name"`
-	Value             types.String `tfsdk:"value"`
-	Min               types.String `tfsdk:"min"`
-	Max               types.String `tfsdk:"max"`
-	Writability       types.String `tfsdk:"writability"`
+	ClusterName       types.String   `tfsdk:"cluster_name"`
+	SettingsProfileID types.String   `tfsdk:"settings_profile_id"`
+	Name              types.String   `tfsdk:"name"`
+	Value             types.String   `tfsdk:"value"`
+	Min               types.String   `tfsdk:"min"`
+	Max               types.String   `tfsdk:"max"`
+	Writability       types.String   `tfsdk:"writability"`
+	Timeouts          timeouts.Value `tfsdk:"timeouts"`
 }
